c258bf55fc604456823e0cbd805501e75b59fcb7
yusei <yuseitbn@gmail.com>
2023-03-08 20:39:41 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yusei-murai/go_practice
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("miss")
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w)
+		}),
+	}
+
+	eg, ctx = errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("miss")
+			return err
+		}
+		return nil
+	})
+
+}
